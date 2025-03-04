1279b85a09fa758135bee5e177e8f94b0bda7b6d
meghaguptaQA <meghagupta22@gmail.com>
2025-03-04 23:06:48 +0530

    Update TestCases.java

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
@@ -85,34 +86,37 @@ public class TestCases {
         Duration timeout=Duration.ofMillis(1000);
         WebDriverWait wait = new WebDriverWait(driver, timeout);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Hockey Teams: Forms, Searching and Pagination']")));
+        
         //Locate the element Hockey Teams: Forms, Searching and Pagination
-        WebElement btnHockey=Wrappers.FindElement(driver,"//a[text()='Hockey Teams: Forms, Searching and Pagination']");
+        // WebElement linkHockey=Wrappers.FindElement(driver,"//a[text()='Hockey Teams: Forms, Searching and Pagination']");
+        WebElement linkHockey=Wrappers.FindElement(driver,"//a[contains(text(),'Hockey Teams')]");
         //Click on the link Hockey Teams: Forms, Searching and Pagination
-        Wrappers.btn_click(btnHockey);
+        Wrappers.btn_click(linkHockey);
         Thread.sleep(3000);
         //Get the team details for win% less tha 0.4 for 4 pages
-        ArrayList<String> Teams = Wrappers.getTeamDetails(driver, 4);
+        ArrayList<HashMap<String,String>> Teams = Wrappers.getTeamDetails(driver, 4,0.4);
         //Create a JSON file
-        Wrappers.CreateJSON(Teams,"JSONFromMapHockey.json");        
+        Wrappers.CreateJSON(Teams,"hockey-team-data.json");        
         System.out.println("end Test case: testCase01");
 
     }
     @Test
     public void testCase02() throws InterruptedException, JsonProcessingException 
     {
-    System.out.println("Start Test case: testCase01");
+    System.out.println("Start Test case: testCase02");
     Duration timeout=Duration.ofMillis(1000);
     WebDriverWait wait = new WebDriverWait(driver, timeout);
     wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Oscar Winning Films: AJAX and Javascript']")));
     //Locate the element Oscar Winning Films: AJAX and Javascript
-    WebElement btnOscar=Wrappers.FindElement(driver,"//a[text()='Oscar Winning Films: AJAX and Javascript']");
+    // WebElement linkOscar=Wrappers.FindElement(driver,"//a[text()='Oscar Winning Films: AJAX and Javascript']");
+    WebElement linkOscar=Wrappers.FindElement(driver,"//a[contains(text(),'Oscar Winning Films')]");
     //Click on Oscar Winning Films: AJAX and Javascript
-    Wrappers.btn_click(btnOscar);
+    Wrappers.btn_click(linkOscar);
     Thread.sleep(3000);
     //Get first 5 films of all the years displayed
-    ArrayList<String> Films = Wrappers.getFilmDetails(driver);
+    ArrayList<HashMap<String,String>> Films = Wrappers.getFilmDetails(driver);
     //Create a JSON file
-    Wrappers.CreateJSON(Films,"JSONFromMapOscars.json");
+    Wrappers.CreateJSON(Films,"oscar-winner-data.json");
     System.out.println("end Test case: testCase02");
 }
 }
